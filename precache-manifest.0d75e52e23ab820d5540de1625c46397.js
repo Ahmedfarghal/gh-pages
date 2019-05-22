b73ad0699f2040a63820539d5cc6ad85ae49888a
Ahmedfarghal <a.fegoo2@gmail.com>
2019-05-22 23:58:36 +0200

    Updates

---
+++ precache-manifest.0d75e52e23ab820d5540de1625c46397.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f861ce8e226fed26d7df25629d5d5b",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "ea43d66641e1e5a9dfe9",
+    "url": "/gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/gh-pages/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "ea43d66641e1e5a9dfe9",
+    "url": "/gh-pages/static/js/main.6421c1fd.chunk.js"
+  },
+  {
+    "revision": "069709dcc3c66b9b2ba3",
+    "url": "/gh-pages/static/js/runtime~main.dcaf1b57.js"
+  }
+]);
